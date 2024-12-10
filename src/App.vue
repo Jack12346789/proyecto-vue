ecdd24f402aac1c00dc0af6385f3f4d4a40ce129
Jack <jackhuamani25@gmail.com>
2024-12-10 18:16:06 -0500

    crea conponente ListaDeTarea, uso de variable reactiva

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,14 @@
 <template>
   <!-- ASI SE LLAMA UN COMPONENTE EN VUE -->
   <Contador></Contador> 
+  <ListaDeTareas></ListaDeTareas>
 </template>
 
 <!-- AQUI SE ESCRIBE TODA LA LOGICA -->
 <script setup>
  import Contador from './components/Contador.vue'; //importando el componente al archivo principal
+ import ListaDeTareas from './components/Lista DeTareas.vue'; //importando el componente al archivo principal
+
 </script>
 
 <!-- ESTILOS -->
---
+++ src/components/Lista DeTareas.vue
@@ -0,0 +1,35 @@
+<!-- CUERPO DEL PROGRAMA -->
+<template>
+    <div>
+        <h2>Lista de Tareas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addTareas"> <!-- Asignamos al input la variable que va a estar cambiando con "v-model"-->
+        <ul>
+            <li v-for="(tarea, index) in tareas" :key="index"> <!--con el v-for recorro todo el vector, y con :key asigno a cada valor de la lista un identificador para manejar los datos de forma eficiente -->
+                {{ tarea  }}
+            </li> 
+        </ul>
+    </div>
+  
+</template>
+  
+<!-- AQUI SE ESCRIBE TODA LA LOGICA -->
+<script setup>
+ import { ref } from 'vue';
+
+ const tareas = ref([]); //lo iniciamos como un arreglo vacio
+ const nuevaTarea = ref('');
+
+ const addTareas = () => {
+    tareas.value.push(nuevaTarea.value); //agreamos el ".push" para agregar una tarea nueva al arreglo
+    nuevaTarea.value = '';
+ }
+
+
+
+</script>
+
+<!-- ESTILOS -->
+<style scoped>
+
+</style>
+  
