4cb5e9732f52d6f04ff8d6b08a91d89fcdde98b7
Jack <jackhuamani25@gmail.com>
2024-12-10 17:43:54 -0500

    CREA COMPONENTE Contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,33 @@
+<!-- CUERPO DEL PROGRAMA -->
+<template>
+  hola
+  <p class="contador">{{ contador }}</p> 
+  <button @click="incrementar">incrementar</button>
+  <button @click="decrementar">decrementar</button>
+
+</template>
+
+<!-- AQUI SE ESCRIBE TODA LA LOGICA -->
+<script setup>
+  import { ref } from 'vue';
+  const contador = ref(0); //variable reactiva
+  
+  //funcion
+  const incrementar = () =>{
+    contador.value ++ ;//asi cambio el valor de la variable o obtrengo su valor
+  }
+
+  const decrementar = () =>{
+    contador.value -- ;//asi cambio el valor de la variable o obtrengo su valor
+  }
+
+</script>
+
+<!-- ESTILOS -->
+<style scoped>
+
+.contador{
+  color: red;
+}
+
+</style>
